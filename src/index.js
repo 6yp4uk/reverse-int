eade5284766bca917622cfe643d0b841282c93f9
6yp4uk <andreiburmagin@gmail.com>
2023-01-15 21:43:30 +0300

    feat:change index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ module.exports = function reverse (n) {
     var num_rtrn = "";
     for (var item in num_str){
       num_rtrn = (num_rtrn + (num_str[(num_str.length - item) - 1]));
-      }
-    if (n < 0){
-        return(0 - Number(num_rtrn))
-      } else if (num_str[num_str.length -1] == 0){
-        return(Number(num_rtrn.padStart(1,0)));
-      } else {
-        return(Number(num_rtrn))
-      } 
-    } 
+    }
+    return (num_rtrn); 
+}
