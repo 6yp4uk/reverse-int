34e1c16be4e66d95846552a028780a6be9c0f554
6yp4uk <andreiburmagin@gmail.com>
2023-01-15 21:40:21 +0300

    feat:commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ module.exports = function reverse (n) {
     num_str = Math.abs(n) + "";
     var num_rtrn = "";
     for (var item in num_str){
-      num_rtrn =num_rtrn + (num_str[(num_str.length - item) - 1]);
-    
-    }
-  
-    console.log(num_rtrn)
+      num_rtrn = (num_rtrn + (num_str[(num_str.length - item) - 1]));
+      }
     if (n < 0){
-      console.log(-num_rtrn)
-    }
-    else {
-      console.log(num_rtrn)
-    }
-}
+        return(0 - Number(num_rtrn))
+      } else if (num_str[num_str.length -1] == 0){
+        return(Number(num_rtrn.padStart(1,0)));
+      } else {
+        return(Number(num_rtrn))
+      } 
+    } 
