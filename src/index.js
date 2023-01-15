a4c48144fd509d5f8612e9b0c23c81b59305c438
6yp4uk <andreiburmagin@gmail.com>
2023-01-15 19:57:42 +0300

    feat:nmp install

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 module.exports = function reverse (n) {
-    num_str = n + "";
-    var num_rtrn = '';
+    num_str = Math.abs(n) + "";
+    var num_rtrn = "";
     for (var item in num_str){
       num_rtrn =num_rtrn + (num_str[(num_str.length - item) - 1]);
+    
     }
-    return(num_rtrn - 0);
+  
+    console.log(num_rtrn)
+    if (n < 0){
+      console.log(-num_rtrn)
+    }
+    else {
+      console.log(num_rtrn)
+    }
 }
